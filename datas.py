4cbdf0d0910df220b48af3156bd25bdbaf70924b
Kunigunda269 <manyk8039@gmail.com>
2024-08-22 07:55:21 -0400

    Add files via upload

---
+++ datas.py
@@ -0,0 +1,33 @@
+# print("hello world") # // - целочисленное деление, / - деление
+# print(type("hello world"))
+# print ('hello,'+'world')    # concatenate (только сложениее строк)
+# # int целое число
+# # float число нецелое
+# # str текст
+# # bool (true/false) логическое
+# print (type(True), type (False))
+# print (5>10, 10>5)
+# print (5==5 or 5<10)
+# x=6579+45/5
+# y='kunigunda + volvA'
+# print (x>y)
+
+# a=23891471923807487.142352314353455
+# b=23891471923807487.142352314356734
+# c=23891471923843245.142352314334563
+# d=23891471923843245.142352314334553
+#
+# m1=a+c
+# m2=b+d
+# print (m1>m2)
+
+# name = 'kunigunda'
+# print (name)
+# name = 'production'
+# print (name)
+
+subject = 'Курс:Python,'
+task_count = 12
+hours_spend =1.5
+average_complete_time = 0.125
+print(subject, 'всего задач:',task_count, ',', 'затрачено часов:',hours_spend, ',', 'среднее время выполнения',average_complete_time,'часа')
