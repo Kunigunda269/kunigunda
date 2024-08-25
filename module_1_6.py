be81c48ad420cd0e4b0b76e6bc9b379763c24bec
Kunigunda269 <manyk8039@gmail.com>
2024-08-25 15:41:02 -0400

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,9 @@
+my_dict = {'sam': 1984, 'sarah': 1915, 'peter':1321}
+print(my_dict)
+print(my_dict['sam'])
+print(my_dict.get('conor'))
+my_dict.update({'nasir': 1855,
+                'fedor': 9930})
+remove = my_dict.pop('sarah')
+print(remove)
+print (my_dict)
