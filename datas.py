b2310db5bffd1bb0d6817ea740eaf4e37866ecc5
Kunigunda269 <manyk8039@gmail.com>
2024-08-25 15:51:01 -0400

    datas.py

--- datas.py
+++ datas.py
@@ -1,6 +1,6 @@
 
-subject = 'Курс:Python,'
+subject = 'Курс: Python,'  
 task_count = 12
-hours_spend =1.5
-average_complete_time = 0.125
-print(subject, 'всего задач:',task_count, ',', 'затрачено часов:',hours_spend, ',', 'среднее время выполнения',average_complete_time,'часа')
+hours_spent = 1.5  
+average_complete_time = hours_spent / task_count  
+print(subject, 'всего задач:', task_count, ',', 'затрачено часов:', hours_spent, ',', 'среднее время выполнения', round(average_complete_time, 3), 'часа')
