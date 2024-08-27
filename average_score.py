43c58d21d9a7fa1d6d33152e0adb5d4db67d262f
Kunigunda269 <manyk8039@gmail.com>
2024-08-27 14:25:01 -0400

    Add files via upload

---
+++ average_score.py
@@ -0,0 +1,10 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+student_list = list(students)
+average_grades = {}
+for i in range(len(student_list)):
+    student = student_list[i]
+    grades_for_student = grades[i]
+    average = sum(grades_for_student) / len(grades_for_student)
+    average_grades[student] = average
+print(average_grades)
