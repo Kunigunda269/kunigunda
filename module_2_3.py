b2ddeb07fedc4c7d8036c9a36fba9fe2b3519550
Kunigunda269 <manyk8039@gmail.com>
2024-08-30 11:40:35 -0400

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,8 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+list_1 = 0
+while list_1 < len(my_list):
+    if my_list[list_1] < 0:
+        break
+    print(my_list[list_1])
+    list_1+=1
+print('список закончен')
