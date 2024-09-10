36e148fb39a8306c333b96a45a2a5debd48d585c
Kunigunda269 <manyk8039@gmail.com>
2024-09-10 08:47:43 -0400

    module_2_hard.py

--- module_2_hard.py
+++ module_2_hard.py
@@ -3,13 +3,14 @@ def generate_password(n):
 
     for i in range(1, n):
         for j in range(i + 1, n):
-            result.append(f'{i}{j}')
-
-    result.sort()
+            pair_sum = i + j
+            if n % pair_sum == 0:
+                result.append(f'{i}{j}')
 
     return ''.join(result)
 
-test_values = [3, 4, 5, 6, 7, 8, 9, 10, 15, 20]
+
+test_values = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 for n in test_values:
     password = generate_password(n)
     print(f"сгенерированный пароль для числа {n}: {password}")
