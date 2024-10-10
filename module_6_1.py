5b05b55a97c3885514a1fdb32a9837d1c1485beb
Kunigunda269 <manyk8039@gmail.com>
2024-10-10 14:22:44 -0400

    module_6_1.py

--- module_6_1.py
+++ module_6_1.py
@@ -1,9 +1,16 @@
-
 class Animal:
     def __init__(self, name):
         self.name = name
         self.alive = True
         self.fed = False
+
+    def eat(self, food):
+        if food.edible:
+            print(f"{self.name} съел {food.name}")
+            self.fed = True
+        else:
+            print(f"{self.name} не стал есть {food.name}")
+            self.alive = False
 
 class Plant:
     def __init__(self, name):
@@ -11,22 +18,10 @@ class Plant:
         self.edible = False
 
 class Mammal(Animal):
-    def eat(self, food):
-        if food.edible:
-            print(f"{self.name} съел {food.name}")
-            self.fed = True
-        else:
-            print(f"{self.name} не стал есть {food.name}")
-            self.alive = False
+    pass
 
 class Predator(Animal):
-    def eat(self, food):
-        if food.edible:
-            print(f"{self.name} съел {food.name}")
-            self.fed = True
-        else:
-            print(f"{self.name} не стал есть {food.name}")
-            self.alive = False
+    pass
 
 class Flower(Plant):
     pass
