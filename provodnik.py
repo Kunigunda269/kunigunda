2fef952d6b6eb0c4bebcbe54ed55e61fb23b4c6d
Kunigunda269 <manyk8039@gmail.com>
2024-10-22 11:51:45 -0400

    Add files via upload

---
+++ provodnik.py
@@ -0,0 +1,50 @@
+import tkinter
+from tkinter import filedialog, messagebox
+import os
+
+def file_select():
+    filename = filedialog.askopenfilename(initialdir='/', title='выберите файл',
+                                          filetypes=(('текстовый файл', 'txt'), ('все файлы', '*')))
+    text['text'] = text['text'] + ' ' + filename
+    os.startfile(filename)
+
+def show_info():
+    info_message = (
+        "Программа для выбора и открытия файлов.\n"
+        "Нажмите 'выбрать файл', чтобы выбрать файл из системы, "
+        "и он будет открыт с помощью приложения по умолчанию."
+    )
+    messagebox.showinfo("Info", info_message)
+
+def show_about():
+    about_message = "Автор: Kunigunda Production\nВерсия: V1_2_3"
+    messagebox.showinfo("About", about_message)
+
+# Настройка главного окна
+window = tkinter.Tk()
+window.title('Проводник')
+window.geometry('350x350')
+window.configure(bg='black')
+window.resizable(False, False)
+
+# Создание меню
+menu_bar = tkinter.Menu(window)
+window.config(menu=menu_bar)
+
+# Добавление пунктов меню
+help_menu = tkinter.Menu(menu_bar, tearoff=0)
+help_menu.add_command(label="Info", command=show_info)
+help_menu.add_command(label="About", command=show_about)
+menu_bar.add_cascade(label="Help", menu=help_menu)
+
+# Отображение текста
+text = tkinter.Label(window, text='Файл', height=5, width=25, background='silver', foreground='blue')
+text.grid(column=1, row=1)
+
+# Кнопка выбора файла
+button_select = tkinter.Button(window, width=20, height=3, text='Выбрать файл', background='silver',
+                               foreground='blue', command=file_select)
+button_select.grid(column=1, row=2, pady=5)
+
+# Запуск главного цикла
+window.mainloop()
