25c062a3fc9de30db1d3002cf94a5220c9f850d9
Kunigunda269 <manyk8039@gmail.com>
2024-10-27 17:04:54 -0400

    dvide_and_rule.py

--- dvide_and_rule.py
+++ dvide_and_rule.py
@@ -20,3 +20,16 @@ def max(list):
 print (sum(list))
 print (count(list))
 print (max(list))
+
+# quicksorting
+def quicksort(array):
+    if len(array) < 2:
+        return array
+    else:
+        pivot = array[0]
+        less = [i for i in array[1:] if i < pivot]
+        greater = [i for i in array[1:] if i > pivot]
+        return quicksort(less) + [pivot] + quicksort(greater)
+
+
+print(quicksort([10, 5, 2, 3]))
